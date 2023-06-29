92d3b12e7d1fedc799f00fc5dce2d6158bbfa431
RoabSouza <roabsouza144@gmail.com>
2023-06-29 18:19:35 -0300

    Adição a classe Marcas

--- src/chorume/Marcas.java
+++ src/chorume/Marcas.java
@@ -2,4 +2,40 @@ package chorume;
 
 public class Marcas {
 
+	final private double COCACOLA = 8.90;
+	private double CocaPreco = 0;
+	private int colacont = 0;
+
+	final private double PEPSI = 7.50;
+	private int pepsicont = 0;
+	private double PepsiPreco = 0;
+
+	final private double ROLLER = 5.50;
+	private int rollercont = 0;
+	private double RollerPreco = 0;
+
+	public void cont(int Ccont) {
+
+		CocaPreco += Ccont;
+		if (CocaPreco >= COCACOLA) {
+			colacont++;
+
+			CocaPreco = 0;
+		}
+		PepsiPreco += Ccont;
+		if (PepsiPreco >= COCACOLA) {
+			colacont++;
+
+			PepsiPreco = 0;
+		}
+
+		rollercont += Ccont;
+		if (rollercont >= COCACOLA) {
+			colacont++;
+
+			rollercont = 0;
+		}
+
+	}
+
 }
